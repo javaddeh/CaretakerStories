1f6ef8bc3a806be59ca562e4a7e99720e19ab304
j.dehdashtian <j.dehdashtian@asax.ir>
2025-04-07 17:28:25 +0330

    added the yaml file.

--- obj/Debug/net9.0/CaretakerStories.AssemblyInfo.cs
+++ obj/Debug/net9.0/CaretakerStories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaretakerStories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b97cde924bcdda2b0db400a031eb3e649e1ee15")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaretakerStories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaretakerStories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
